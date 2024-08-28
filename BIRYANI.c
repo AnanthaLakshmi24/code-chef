82061925389b2d35c2df36c889bf362e0119004e
G.Durga Anantha Lakshmi <166210110+AnanthaLakshmi24@users.noreply.github.com>
2024-08-28 19:45:46 +0530

    Add files via upload

--- BIRYANI.c
+++ BIRYANI.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 int main(void) {
-    int t;
-    scanf("%d",&t);
-    while(t--)
-    {
-        int x,y,z;
-        scanf("%d %d",&x,&y);
-        z=(x*y);
-        printf("%d\n",z);
-    }
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int x,y,z;
+	    scanf("%d%d",&x,&y);
+	    z=x*y;
+	    printf("%d\n",z);
+	}
 
 }
 
